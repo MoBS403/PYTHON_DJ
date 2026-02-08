c5e1fc9a6f340dafe21568c5c3366daef2e465f6
brumab <brunomolinasouza1986@gmail.com>
2026-02-08 14:47:32 -0300

    Deploy Django ready for create 4

--- management/commands/create_admin.py
+++ management/commands/create_admin.py
@@ -5,18 +5,19 @@ import os
 User = get_user_model()
 
 class Command(BaseCommand):
+    help = "Cria superuser automaticamente usando variáveis de ambiente"
+
     def handle(self, *args, **kwargs):
         username = os.getenv("ADMIN_USERNAME")
         email = os.getenv("ADMIN_EMAIL")
         password = os.getenv("ADMIN_PASSWORD")
 
         if not username or not password:
+            self.stdout.write(self.style.WARNING("ADMIN_USERNAME ou ADMIN_PASSWORD não definido"))
             return
 
         if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
-            print("Superuser criado com sucesso")
+            User.objects.create_superuser(username=username, email=email, password=password)
+            self.stdout.write(self.style.SUCCESS(f"Superuser {username} criado com sucesso"))
+        else:
+            self.stdout.write(self.style.NOTICE(f"Superuser {username} já existe"))
